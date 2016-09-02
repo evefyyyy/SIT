2bd418482e4960db4f18b1c67f17011b4167837e
evefyyyy <evefyjubpajub@gmail.com>
2016-09-02 18:25:45 +0700

    update editProject

--- resources/views/student/editProject.blade.php
+++ resources/views/student/editProject.blade.php
@@ -2,16 +2,16 @@
 @section('content')
 	<div id="detail">
 	<div class="row">
-		<div class="hidden-xs col-md-1 col-lg-1"></div>
-		<div class="col-xs-12 col-md-10 col-lg-10">
+		<div class="col-xs-1 col-sm-1 col-md-1 col-lg-1"></div>
+		<div class="col-xs-10 col-sm-10 col-md-10 col-lg-10">
 			<h3>Driving simulation</h3>
 			<h4>เกมจำลองสถานการณ์การสอบใบอนุญาติขับขี่รถยนต์</h4>
 		</div>
-		<div class="hidden-xs col-md-1 col-lg-1"></div>
+		<div class="col-xs-1 col-sm-1 col-md-1 col-lg-1"></div>
 	</div>
 	<div class="row">
-		<div class="hidden-xs col-md-1 col-lg-1"></div>
-		<div class="col-xs-12 col-md-10 col-lg-10">
+		<div class="col-xs-1 col-sm-1 col-md-1 col-lg-1"></div>
+		<div class="col-xs-10 col-sm-10 col-md-10 col-lg-10">
 			<div class="panel panel-info">
 			  <div class="panel-heading">details</div>
 			  <div class="panel-body">
@@ -21,12 +21,15 @@
 			  </div>
 			</div>
 		</div>
-		<div class="hidden-xs col-md-1 col-lg-1"></div>
+		<div class="col-xs-1 col-sm-1 col-md-1 col-lg-1"></div>
 	</div>
 	<div class="row">
-		<div class="hidden-xs col-md-1 col-lg-1"></div>
-		<div class="col-xs-12 col-md-5 col-lg-5"></div>
-		<div class="col-xs-12 col-md-5 col-lg-5">
+		<div class="col-xs-1 col-sm-1 col-md-1 col-lg-1"></div>
+		<div class="col-xs-10 col-sm-10 col-md-5 col-lg-5"></div>
+		<div class="col-xs-1 col-sm-1 hidden-md hidden-lg"></div>
+		<div class="col-xs-12 col-sm-12 hidden-md hidden-lg"></div>
+		<div class="col-xs-1 col-sm-1 hidden-md hidden-lg"></div>
+		<div class="col-xs-10 col-sm-10 col-md-5 col-lg-5">
 		<div class="panel panel-info">
 			<div class="panel-heading">author</div>
 			<div class="panel-body">
@@ -45,21 +48,26 @@
 			</div>
 		</div>
 		</div>
-		<div class="hidden-xs col-md-1 col-lg-1"></div>
+		<div class="col-xs-1 col-sm-1 col-md-1 col-lg-1"></div>
 	</div>
 	<div class="row">
-		<div class="hidden-xs col-md-1 col-lg-1"></div>
-		<div class="col-xs-12 col-md-5 col-lg-5">
+		<div class="col-xs-1 col-sm-1 col-md-1 col-lg-1"></div>
+		<div class="col-xs-10 col-sm-10 col-md-5 col-lg-5">
 		<div class="panel panel-info">
 			<div class="panel-heading">tools & techniques</div>
 			<div class="panel-body">
+			<div class="col-lg-12">
 			<form class="form-inline editableform text">
 			    <a href="#" id="tools" data-type="textarea">click here for an example</a>
 			</form>
 			</div>
+			</div>
 		</div>
 		</div>
-		<div class="col-xs-12 col-md-5 col-lg-5">
+		<div class="col-xs-1 col-sm-1 hidden-md hidden-lg"></div>
+		<div class="col-xs-12 col-sm-12 hidden-md hidden-lg"></div>
+		<div class="col-xs-1 col-sm-1 hidden-md hidden-lg"></div>
+		<div class="col-xs-10 col-sm-10 col-md-5 col-lg-5">
 		<div class="panel panel-info">
 			<div class="panel-heading">advisor</div>
 			<div class="panel-body">
@@ -68,7 +76,7 @@
 			</div>
 		</div>
 		</div>
-		<div class="hidden-xs col-md-1 col-lg-1"></div>
+		<div class="col-xs-1 col-sm-1 col-md-1 col-lg-1"></div>
 	</div>
 	</div>
 	<script src="{!! URL::asset('js/edit.js') !!}"></script>
