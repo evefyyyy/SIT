b09bacb169f08999286f9626c5ecb808f70b03ca
llfangll <fang.ronaporn@gmail.com>
2016-09-07 20:17:59 +0700

    update show project

--- resources/views/admin/approveProject.blade.php
+++ resources/views/admin/approveProject.blade.php
@@ -1,96 +1,106 @@
 @extends('adminTmp')
 @section('content')
-	<script type="text/javascript">
-    		function DataTo(value, id) {
-    			
-    			document.getElementById('btn'+id).setAttribute("onclick","window.location.href = 'pending/approve/"+id+"/"+value+"'");
+<script type="text/javascript">
+function DataTo(value, id) {
+
+	document.getElementById('btn'+id).setAttribute("onclick","window.location.href = 'pending/approve/"+id+"/"+value+"'");
     			//alert("window.location.href = 'pending/approve/'"+id+"/"+value);
     		}
 
     		</script>
-		<table class="table" id="projectTB">
-		<thead>
-			<tr>
-				<th>
-					{{$countProject}} Pending Projects
-				<span class="allpro"><a href="/project">all projects</a></span>
-				</th>
-			</tr>
-		</thead>
-		<tbody>
-		@if($countProject===0)
-		<tr>
-			<td colspan="4" class="no-project">no pending project</td>
-		</tr>
-		@else
-		@foreach($project as $pj)
-		@if($pj->groupProject->group_project_approve===0)
-		 <tr>
-	        <table class="table pending">
-	          <tr>
-			    <th rowspan="2" style="width:15%">Project name<span>:</span></th>
-			    <td colspan="3" rowspan="2" style="width:55%" id="name">
-			    	{{$pj->groupProject->group_project_eng_name}}
-			    	<br>{{$pj->groupProject->group_project_th_name}}
-			    </td>
-			    <td rowspan="2" colspan="3" style="width:30%" id="proid">
-			    	<button id="delpj" class="rejectbt cd-popup-trigger" value="{{$pj->groupProject->id}}">reject</button>
-			    	<button id="btn{{$pj->groupProject->id}}" class="delete approvebt">approve</button>
-			    	<input id="proid{{$pj->groupProject->id}}" onblur="DataTo(this.value, {{$pj->groupProject->id}});" type="text" class="form-control" placeholder="project ID" />
-			    </td>
-			  </tr>
-			  <tr>
-			  </tr>
-			  <tr>
-			    <th>Type<span>:</span></th>
-			    <td style="width:15%">{{$pj->groupProject->type->type_name}}</td>
-			    <th style="width:15%">Category<span>:</span></th>
-			    <td style="width:15%">{{$pj->groupProject->category->category_name}}</td>
-			    <td></td>
-			    <td><a href="#" download><div class="glyphicon glyphicon-download"></div> download proposal</a></td>
-			  </tr>
-			  <tr>
-			    <th rowspan="3">Team member<span>:</span></th>
-			    <?php $teams = App\ProjectStudent::where('project_pkid', $pj->project_pkid)->get(); ?>
-			    <td>
-			    @foreach($teams as $team)
-			    	{{$team->student->student_id}}<br>
-			    @endforeach
-			    </td>
-			    <td colspan="2">
-			    @foreach($teams as $team)
-			    	{{ $team->student->student_fname." ".$team->student->student_lname }}<br>
-			    @endforeach
-			    </td>
-			    <?php $advisors = App\ProjectAdvisor::where('project_pkid', $pj->project_pkid)->get(); ?>
-			    <th>Advisor<span>:</span></th>
-			    <td>
-			     @foreach($advisors as $key => $advisor)
-			    	{{ $advisor->advisor->advisor_fname." ".$advisor->advisor->advisor_lname }}@if($key < 1)<label id="main">Main</label>@endif<br>
-			    @endforeach
-			    </td>
-			  </tr>
-	        </table>
-	      </tr>
-	      
-    	<script src="{!! URL::asset('js/approve.js') !!}"></script>
-	      @endif
-	      @endforeach
-	      @endif
-		    </tbody>
-			</table>
+    	<div class="row">
+    		<div id="projectTB">
+    			<table class="table">
+    				<thead>
+    					<tr>
+    						<th colspan="4">
+    							{{$countProject}} Pending Projects
+    						<span class="approveall">
+    							<a href="#">approve all</a><i>|</i><a class="back" href="/project">back to approved projects</a>
+    						</span>
+    						</th>
+    					</tr>
+    				</thead>
+    				<tbody>
+    					@if($countProject===0)
+    					<tr>
+    						<td colspan="4" class="no-project">no pending project</td>
+    					</tr>
+    					@else
+    					@foreach($project as $pj)
+    					@if($pj->groupProject->group_project_approve===0)
+    					<tr>
+    						<table class="table pending">
+    							<tr>
+    								<th rowspan="2" style="width:15%">Project name<span>:</span></th>
+    								<td colspan="3" rowspan="2" style="width:55%" id="name">
+    									{{$pj->groupProject->group_project_eng_name}}
+    									<br>{{$pj->groupProject->group_project_th_name}}
+    								</td>
+    								<td rowspan="2" colspan="3" style="width:30%" id="proid">
+    									<button id="delpj" class="rejectbt cd-popup-trigger" value="{{$pj->groupProject->id}}">reject</button>
+    									<button id="btn{{$pj->groupProject->id}}" class="delete approvebt">approve</button>
+    									<label id="proid{{$pj->groupProject->id}}" onblur="DataTo(this.value, {{$pj->groupProject->id}});">IT56-bu01</label>
+    								</td>
+    							</tr>
+    							<tr>
+    							</tr>
+    							<tr>
+    								<th>Type<span>:</span></th>
+    								<td style="width:15%">{{$pj->groupProject->type->type_name}}</td>
+    								<th style="width:15%">Category<span>:</span></th>
+    								<td style="width:15%">{{$pj->groupProject->category->category_name}}</td>
+    								<td></td>
+    								<td><a href="#" download><div class="glyphicon glyphicon-download"></div> download proposal</a></td>
+    							</tr>
+    							<tr>
+    								<th rowspan="2">Team member<span>:</span></th>
+    								<?php $teams = App\ProjectStudent::where('project_pkid', $pj->project_pkid)->get(); ?>
+    								<td rowspan="2">
+    									@foreach($teams as $team)
+    									{{$team->student->student_id}}<br>
+    									@endforeach
+    								</td>
+    								<td rowspan="2" colspan="2">
+    									@foreach($teams as $team)
+    									{{ $team->student->student_fname." ".$team->student->student_lname }}<br>
+    									@endforeach
+    								</td>
+    								<?php $advisors = App\ProjectAdvisor::where('project_pkid', $pj->project_pkid)->get(); ?>
+    								<th>main advisor<span>:</span></th>
+    								<td>
+    									@foreach($advisors as $key => $advisor)
+    									{{ $advisor->advisor->advisor_fname." ".$advisor->advisor->advisor_lname }}<br>
+    									@endforeach
+    								</td>
+    							</tr>
+    							<tr><th>co-advisor<span>:<span></th>
+    								<td>
+    									<!-- co-advisor name -->
+    								</td>
+    							</tr>
+    						</table>
+    					</tr>
 
-		<div class="cd-popup" role="alert">
-			<div class="cd-popup-container">
-				<p>Are you sure you want to reject this project?</p>
-				<ul class="cd-buttons">
-					<li><a class="cd-delete">Yes</a></li>
-					<li><a class="cd-close">No</a></li>
-				</ul>
-				<a class="cd-popup-close cd-close img-replace"></a>
-			</div> <!-- cd-popup-container -->
-		</div> <!-- cd-popup -->
-		
-		
+    					<script src="{!! URL::asset('js/approve.js') !!}"></script>
+    					@endif
+    					@endforeach
+    					@endif
+    				</tbody>
+    			</table>
+    		</div>
+    	</div>
+    			<div class="cd-popup" role="alert">
+    				<div class="cd-popup-container">
+    					<p>Are you sure you want to reject this project?</p>
+    					<ul class="cd-buttons">
+    						<li><a class="cd-delete">Yes</a></li>
+    						<li><a class="cd-close">No</a></li>
+    					</ul>
+    					<a class="cd-popup-close cd-close img-replace"></a>
+    				</div> <!-- cd-popup-container -->
+    			</div> <!-- cd-popup -->
 
-@stop
+
+
+    			@stop
--- resources/views/admin/doc.blade.php
+++ resources/views/admin/doc.blade.php
@@ -1,30 +1,30 @@
 @extends('adminTmp')
 @section('content')
-	<div class="row news-head">
-		<div class="hidden-xs col-md-1 col-lg-1"></div>
-		<div class="col-xs-12 col-md-10 col-lg-10">
-			<img height="45" src="/img/document.png"><label>documents</label>
-			<span id="pendlink">
-        	 	<a class="btn" data-toggle="modal" data-target="#addDoc"><i class="glyphicon glyphicon-plus" data-toggle="modal" data-target="#addDoc"></i>document</a>
-    		</span>
-		</div>
-		<div class="hidden-xs col-md-1 col-lg-1"></div>
+<div class="row news-head">
+	<div class="hidden-xs col-md-1 col-lg-1"></div>
+	<div class="col-xs-12 col-md-10 col-lg-10">
+		<img height="45" src="/img/document.png"><label>documents</label>
+		<span id="pendlink">
+			<a class="btn" data-toggle="modal" data-target="#addDoc"><i class="glyphicon glyphicon-plus" data-toggle="modal" data-target="#addDoc"></i>document</a>
+		</span>
 	</div>
-	<div class="row" id="newTB">
-		<table class="table table-responsive table-hover">
-			<thead>
-				<tr>
-					<th colspan="2" style="width:70%">title</th>
-					<th style-"width:15%">download file</th>
-					<th style="width:15%">date</th>
-				</tr>
-			</thead>
-			<tbody>
-				<!-- no document -->
+	<div class="hidden-xs col-md-1 col-lg-1"></div>
+</div>
+<div class="row" id="newTB">
+	<table class="table table-responsive table-hover">
+		<thead>
+			<tr>
+				<th colspan="2" style="width:70%">title</th>
+				<th style-"width:15%">download file</th>
+				<th style="width:15%">date</th>
+			</tr>
+		</thead>
+		<tbody>
+			<!-- no document -->
 				<!-- <tr>
 					<td colspan="4" class="no-project">There is no document.</td>
 				</tr> -->
-			@foreach ($news as $n)
+				@foreach ($news as $n)
 				<tr>
 					<td><a data-toggle="modal" data-target="#doc{{$count}}">{{$n->title}}</a></td>
 					<td style="width:10%">
@@ -38,102 +38,125 @@
 					<td><a href="/adminNewsFiles/{{$n->file_path_name}}" download><i class="flaticon-doc-file-format-symbol"></i></a></td>
 					<td>{{date('F d, Y',strtotime($n->created_at))}}</td>
 				</tr>
-			@endforeach
+				@endforeach
 			</tbody>
 		</table>
 	</div>
 	<?php 
-		$count = 0 ;
+	$count = 0 ;
 	?>
 	@foreach ($news as $n)
 	<!-- edit document for admin -->
-	 <div class="modal fade" id="doc{{$count}}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel">
-	  <div class="modal-dialog" role="document">
-	    <div class="modal-content">
-	      <div class="modal-header">
-	      	<form method="post" action="/news/document/edit" enctype="multipart/form-data">
-	        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button> 
-	        <input type="text" class="form-control" id="title{{$count}}" name="title" value="{{$n->title}}" onkeyup="copy({{$count}})" required>
-	      </div>
-	      <div class="modal-body">
-	        	<input type="hidden" id="_token" name="_token" value="{{ csrf_token() }}">
-	          <div class="form-group">
-	            <label for="message-text" class="control-label">File</label>
-	            <span>
-				    <input type="file" id="file" name="myfiles" required/>
-				</span>
-				<br/>
-	          </div>
-	          	<input type="hidden" name="hId" value="{{$n->id}}">
-	          	<input type="hidden" name="cTitle" id="copy{{$count++}}" value="{{$n->title}}">
-	      </div>
-	      <div class="modal-footer">
-	        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-	        <button type="submit" class="btn btn-primary">save</button>
-	        </form>
-	      </div>
-	    </div>
-	  </div>
+	<div class="modal fade" id="doc{{$count}}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel">
+		<div class="modal-dialog" role="document">
+			<div class="modal-content">
+				<div class="modal-header">
+					<form method="post" action="/news/document/edit" enctype="multipart/form-data">
+						<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button> 
+						<input type="text" class="form-control" id="title{{$count}}" name="title" value="{{$n->title}}" onkeyup="copy({{$count}})" required>
+					</div>
+					<div class="modal-body">
+						<input type="hidden" id="_token" name="_token" value="{{ csrf_token() }}">
+						<div class="form-group">
+							<label for="message-text" class="control-label">File</label>
+							<span>
+								<input type="file" id="file" name="myfiles" required/>
+							</span>
+							<br/>
+						</div>
+						<div class="form-group" style="width:40%">
+							<label for="message-text" class="control-label">Expiration date</label>
+							<div class='input-group date datetimepicker'>
+								<input type='text' class="form-control" />
+								<span class="input-group-addon">
+									<span class="glyphicon glyphicon-calendar"></span>
+								</span>
+							</div>
+						</div>
+						<input type="hidden" name="hId" value="{{$n->id}}">
+						<input type="hidden" name="cTitle" id="copy{{$count++}}" value="{{$n->title}}">
+					</div>
+					<div class="modal-footer">
+						<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+						<button type="submit" class="btn btn-primary">save</button>
+					</form>
+				</div>
+			</div>
+		</div>
 	</div> 
 	@endforeach
 	<!-- add a document for admin -->
 	<div class="modal fade" id="addDoc" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel">
-	  <div class="modal-dialog" role="document">
-	    <div class="modal-content">
-	      <div class="modal-header">
-	        <button type="button" class="close" data-dismiss="modal" aria-label="Close"></button>
-	        <h4 class="modal-title">New Document</h4>
-	      </div>
-	      <div class="modal-body">
-	        <form method="post" action="/news/document" enctype="multipart/form-data">
-	        	<input type="hidden" id="_token" name="_token" value="{{ csrf_token() }}">
-	          <div class="form-group">
-	            <label for="recipient-name" class="control-label">Title</label>
-	            <input type="text" class="form-control" id="title" name="title" required/>
-	           </div>
-	          <div class="form-group">
-	            <label for="message-text" class="control-label">File</label>
-				    <input type="file" id="file" name="myfiles" required/>
-				<br/>
-	       	   </div>
-	       </div>
-	      <div class="modal-footer">
-	        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-	        <button type="submit" class="btn btn-primary">add</button>
-	        </form>
-	      </div>
-	    </div><!-- /.modal-content -->
-	  </div><!-- /.modal-dialog -->
+		<div class="modal-dialog" role="document">
+			<div class="modal-content">
+				<div class="modal-header">
+					<button type="button" class="close" data-dismiss="modal" aria-label="Close"></button>
+					<h4 class="modal-title">New Document</h4>
+				</div>
+				<div class="modal-body">
+					<form method="post" action="/news/document" enctype="multipart/form-data">
+						<input type="hidden" id="_token" name="_token" value="{{ csrf_token() }}">
+						<div class="form-group">
+							<label for="recipient-name" class="control-label">Title</label>
+							<input type="text" class="form-control" id="title" name="title" required/>
+						</div>
+						<div class="form-group">
+							<label for="message-text" class="control-label">File</label>
+							<input type="file" id="file" name="myfiles" required/>
+							<br/>
+						</div>
+						<div class="form-group" style="width:40%">
+							<label for="message-text" class="control-label">Expiration date</label>
+							<div class='input-group date datetimepicker'>
+								<input type='text' class="form-control" />
+								<span class="input-group-addon">
+									<span class="glyphicon glyphicon-calendar"></span>
+								</span>
+							</div>
+						</div>
+					</div>
+					<div class="modal-footer">
+						<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+						<button type="submit" class="btn btn-primary">add</button>
+					</form>
+				</div>
+			</div><!-- /.modal-content -->
+		</div><!-- /.modal-dialog -->
 	</div><!-- /.modal -->
 	<script src="{!! URL::asset('js/create.js') !!}"></script>
-	<script>
-		$('[data-toggle=confirmation]').confirmation({
-				rootSelector: '[data-toggle=confirmation]',
-				placement: 'top',
-				onConfirm: function() {
-					var num = $("#num").val() ;
-					$.ajax({
-						type:"post",
-						dataType: "",
-						url :"/news/delete",
-						data: {id: $("#nId"+num).val(),type: $("#type").val() , _token:$("#_token").val() },
-						success:function(data){
-							if(data == 'd'){
-								window.location.pathname = "/news/document/";
-							}else{
-								window.location.pathname = "/news/announcement/";
-							}
+	<script type="text/javascript">
+	$(function () {
+				$('.datetimepicker').datetimepicker({
+					format: 'DD/MM/YYYY'
+				});
+			});
+	$('[data-toggle=confirmation]').confirmation({
+		rootSelector: '[data-toggle=confirmation]',
+		placement: 'top',
+		onConfirm: function() {
+			var num = $("#num").val() ;
+			$.ajax({
+				type:"post",
+				dataType: "",
+				url :"/news/delete",
+				data: {id: $("#nId"+num).val(),type: $("#type").val() , _token:$("#_token").val() },
+				success:function(data){
+					if(data == 'd'){
+						window.location.pathname = "/news/document/";
+					}else{
+						window.location.pathname = "/news/announcement/";
+					}
 
-						}
-					});
 				}
-		});
-		function copy(x) {
-			$y = $("#title"+x).val() ;
-			document.getElementById('copy'+x).value = $y;			
+			});
 		}
-		function setNum(x){
-			document.getElementById('num').value = x;			
-		}
+	});
+	function copy(x) {
+		$y = $("#title"+x).val() ;
+		document.getElementById('copy'+x).value = $y;			
+	}
+	function setNum(x){
+		document.getElementById('num').value = x;			
+	}
 	</script>
-@stop
+	@stop
