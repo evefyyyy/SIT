bf6484da7b11d232e642e72ee48ace7df67761d2
evefyyyy <evefyjubpajub@gmail.com>
2016-09-19 13:19:08 +0700

    push

--- resources/views/advTmp.blade.php
+++ resources/views/advTmp.blade.php
@@ -34,7 +34,7 @@
                   <li><a href="/index">Back to homepage</a></li>
                 </ul>
                 <ul class="nav navbar-nav navbar-right">
-                <p class="navbar-text navbar-right"><img height="18" src="/img/user.png"> <span class="firstname">Umaporn</span><span class="lol">|</span><a href="/logout" class="navbar-link logout">Logout</a></p>
+                <p class="navbar-text navbar-right"><img height="18" src="/img/user.png"> <span class="firstname">Umaporn</span><span class="lol">|</span><a href="/index" class="navbar-link logout">Logout</a></p>
               </ul>
           </nav>
         </div>
