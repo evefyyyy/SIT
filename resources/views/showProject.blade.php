fe9d730281970b2e1a87e55507d657aa91a60583
llfangll <fang.ronaporn@gmail.com>
2016-09-19 14:03:54 +0700

    showproject

--- resources/views/showProject.blade.php
+++ resources/views/showProject.blade.php
@@ -17,9 +17,11 @@
 			<h4>{{$projectNameTH}}</h4>
 		</div>
 		<div class="col-xs-3 col-sm-3 col-md-3 col-lg-3 btn-save">
+			@if(Auth::check())
 		<form class="" action="{{url('student/myproject/edit/'.$checkProject.'/edit')}}" method="get">
 			<button style="float:right" class="btn btn-browse" onclick="window.location.href='/student/myproject/edit'">edit my project</button>
 		</form>
+		@endif
 		</div>
 		<div class="col-xs-1 col-sm-1 col-md-1 col-lg-1"></div>
 	</div>
