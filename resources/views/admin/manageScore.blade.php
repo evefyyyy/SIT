8819e537fc906b8ed9b5280956eb99a66ccb7f4f
llfangll <fang.ronaporn@gmail.com>
2016-10-10 12:56:12 +0700

    pull pull

--- resources/views/admin/manageScore.blade.php
+++ resources/views/admin/manageScore.blade.php
@@ -108,21 +108,6 @@
 <div class="col-xs-1 col-md-3 col-lg-3"></div>
 </div>
 </div>
-<div class="2 box">
-  <div class="row">
-    <div class="col-xs-1 col-md-3 col-lg-3"></div>
-    <div class="col-xs-10 col-md-6 col-lg-6">
-      template 2
-</div>
-<div class="col-xs-1 col-md-3 col-lg-3"></div>
-</div>
-</div>
-<div class="3 box">
-  <div class="row">
-    <div class="col-xs-1 col-md-3 col-lg-3"></div>
-    <div class="col-xs-10 col-md-6 col-lg-6">
-      template 3
-</div>
 <div class="col-xs-1 col-md-3 col-lg-3"></div>
 </div>
 </div>
