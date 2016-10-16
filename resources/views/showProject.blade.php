d298a638d6338ffdbaaa9addcb92b59d3c562bc9
nookotto <undernook@hotmail.com>
2016-10-16 16:32:26 +0700

    fix showproject

--- resources/views/showProject.blade.php
+++ resources/views/showProject.blade.php
@@ -20,16 +20,12 @@
 	</div>
 	<div class="row">
 		<div class="col-xs-1 col-sm-1 col-md-1 col-lg-1"></div>
-		<div class="col-xs-10 col-sm-10 col-md-10 col-lg-10">
-			<h3>{{$projectNameEN}}</h3>
-			<h4>{{$projectNameTH}}</h4>
-		</div>
 		@if(Auth::check())
-			<?php
-				$objs = Auth::user()->user_student->student->student_id;
-		    $checkStd = DB::table('students')->where('student_id',$objs)->value('id');
-		    $projectId = DB::table('project_students')->where('student_pkid',$checkStd)->value('project_pkid');
-			?>
+		<?php
+		$objs = Auth::user()->user_student->student->student_id;
+		$checkStd = DB::table('students')->where('student_id',$objs)->value('id');
+		$projectId = DB::table('project_students')->where('student_pkid',$checkStd)->value('project_pkid');
+		?>
 		<div class="col-xs-7 col-sm-7 col-md-7 col-lg-7">
 			<h3>{{$projectNameEN}}</h3>
 			<h4>{{$projectNameTH}}</h4>
@@ -37,12 +33,18 @@
 		<div class="col-xs-3 col-sm-3 col-md-3 col-lg-3 btn-save">
 
 			
-			 @if($projectId == $checkProject)
-		<form action="{{url('student/myproject/edit/'.$checkProject.'/edit')}}" method="get">
-			<button style="float:right" class="btn btn-browse" onclick="window.location.href='/student/myproject/edit'">edit my project</button>
-		</form>
+			@if($projectId == $checkProject)
+			<form action="{{url('student/myproject/edit/'.$checkProject.'/edit')}}" method="get">
+				<button style="float:right" class="btn btn-browse" onclick="window.location.href='/student/myproject/edit'">edit my project</button>
+			</form>
 		</div>
 		@endif
+		@else
+		<div class="col-xs-10 col-sm-10 col-md-10 col-lg-10">
+			<h3>{{$projectNameEN}}</h3>
+			<h4>{{$projectNameTH}}</h4>
+		</div>
+
 		@endif
 		<div class="col-xs-1 col-sm-1 col-md-1 col-lg-1"></div>
 	</div>
@@ -50,12 +52,12 @@
 		<div class="col-xs-1 col-sm-1 col-md-1 col-lg-1"></div>
 		<div class="col-xs-10 col-sm-10 col-md-10 col-lg-10">
 			<div class="panel panel-info">
-			  <div class="panel-heading">details</div>
-			  <div class="panel-body">
-			  	<div class="col-lg-12 text">
-			    	  {{$detail}}
+				<div class="panel-heading">details</div>
+				<div class="panel-body">
+					<div class="col-lg-12 text">
+						{{$detail}}
+					</div>
 				</div>
-			  </div>
 			</div>
 		</div>
 		<div class="col-xs-1 col-sm-1 col-md-1 col-lg-1"></div>
@@ -74,136 +76,136 @@
 					<div class="col-lg-12 tools">{!!$tools!!}</div>
 				</div>
 			</div>
-    @if($video != null)
-		<div id='ninja-slider'>
-        <div>
-            <div class="slider-inner">
-                <ul>
-										@foreach($screenshot as $img)
-                    <li><a class="ns-img" href="{{$img->picture_path_name}}"></a></li>
-										@endforeach
-                </ul>
-                <div class="fs-icon" title="Expand/Close"></div>
-            </div>
-            <div id="thumbnail-slider">
-                <div class="inner">
-                    <ul>
-												@foreach($screenshot as $img)
-                        <li>
-                            <a class="thumb" href="{{$img->picture_path_name}}"></a>
-                        </li>
-												@endforeach
-                    </ul>
-                </div>
-            </div>
-        </div>
-   		</div>
-      @endif
+			@if($video != null)
+			<div id='ninja-slider'>
+				<div>
+					<div class="slider-inner">
+						<ul>
+							@foreach($screenshot as $img)
+							<li><a class="ns-img" href="{{$img->picture_path_name}}"></a></li>
+							@endforeach
+						</ul>
+						<div class="fs-icon" title="Expand/Close"></div>
+					</div>
+					<div id="thumbnail-slider">
+						<div class="inner">
+							<ul>
+								@foreach($screenshot as $img)
+								<li>
+									<a class="thumb" href="{{$img->picture_path_name}}"></a>
+								</li>
+								@endforeach
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
+			@endif
 		</div>
 		<div class="col-xs-1 col-sm-1 hidden-md hidden-lg"></div>
 		<div class="col-xs-12 col-sm-12 hidden-md hidden-lg"></div>
 		<div class="col-xs-1 col-sm-1 hidden-md hidden-lg"></div>
 		<div class="col-xs-10 col-sm-10 col-md-5 col-lg-5">
-		<div class="panel panel-info">
-			<div class="panel-heading">author</div>
-			<div class="panel-body">
-			<div class="col-xs-6 col-md-6 col-lg-6 text">{{$stdName1}}</div>
-			<div class="col-xs-6 col-md-6 col-lg-6 text">รหัสนักศึกษา {{$stdId1}}</div>
-			<div class="col-xs-6 col-md-6 col-lg-6"></div>
-			<div class="col-xs-6 col-md-6 col-lg-6 mail"><img height="11" src="/img/email.png"> {{$email1}}</div>
-			<div class="col-xs-6 col-md-6 col-lg-6 text">{{$stdName2}}</div>
-			<div class="col-xs-6 col-md-6 col-lg-6 text">รหัสนักศึกษา {{$stdId2}}</div>
-			<div class="col-xs-6 col-md-6 col-lg-6"></div>
-			<div class="col-xs-6 col-md-6 col-lg-6 mail"><img height="11" src="/img/email.png"> {{$email2}}</div>
-      @if(count($student) == 3)
-			<div class="col-xs-6 col-md-6 col-lg-6 text">{{$stdName3}}</div>
-			<div class="col-xs-6 col-md-6 col-lg-6 text">รหัสนักศึกษา {{$stdId3}}</div>
-			<div class="col-xs-6 col-md-6 col-lg-6"></div>
-			<div class="col-xs-6 col-md-6 col-lg-6 mail"><img height="11" src="/img/email.png"> {{$email3}}</div>
-      @endif
+			<div class="panel panel-info">
+				<div class="panel-heading">author</div>
+				<div class="panel-body">
+					<div class="col-xs-6 col-md-6 col-lg-6 text">{{$stdName1}}</div>
+					<div class="col-xs-6 col-md-6 col-lg-6 text">รหัสนักศึกษา {{$stdId1}}</div>
+					<div class="col-xs-6 col-md-6 col-lg-6"></div>
+					<div class="col-xs-6 col-md-6 col-lg-6 mail"><img height="11" src="/img/email.png"> {{$email1}}</div>
+					<div class="col-xs-6 col-md-6 col-lg-6 text">{{$stdName2}}</div>
+					<div class="col-xs-6 col-md-6 col-lg-6 text">รหัสนักศึกษา {{$stdId2}}</div>
+					<div class="col-xs-6 col-md-6 col-lg-6"></div>
+					<div class="col-xs-6 col-md-6 col-lg-6 mail"><img height="11" src="/img/email.png"> {{$email2}}</div>
+					@if(count($student) == 3)
+					<div class="col-xs-6 col-md-6 col-lg-6 text">{{$stdName3}}</div>
+					<div class="col-xs-6 col-md-6 col-lg-6 text">รหัสนักศึกษา {{$stdId3}}</div>
+					<div class="col-xs-6 col-md-6 col-lg-6"></div>
+					<div class="col-xs-6 col-md-6 col-lg-6 mail"><img height="11" src="/img/email.png"> {{$email3}}</div>
+					@endif
+				</div>
 			</div>
-		</div>
-		<div class="panel panel-info">
-			<div class="panel-heading">advisor</div>
-			<div class="panel-body">
-			@foreach($advisors as $adv)
-			<div class="col-lg-12 text">{{$adv->advisor_name}}</div>
-			@endforeach
+			<div class="panel panel-info">
+				<div class="panel-heading">advisor</div>
+				<div class="panel-body">
+					@foreach($advisors as $adv)
+					<div class="col-lg-12 text">{{$adv->advisor_name}}</div>
+					@endforeach
+				</div>
 			</div>
-		</div>
-    @if($video != null)
-		<div class="embed-responsive embed-responsive-16by9">
-			{!! $video !!}
-		</div>
+			@if($video != null)
+			<div class="embed-responsive embed-responsive-16by9">
+				{!! $video !!}
+			</div>
 		</div>
 		<div class="col-xs-1 col-sm-1 col-md-1 col-lg-1"></div>
 	</div>
-  @else
+	@else
 	<!-- in case no video -->
-<div class="row">
+	<div class="row">
 		<div class="col-xs-1 col-sm-1 col-md-1 col-lg-1"></div>
 		<div class="col-xs-10 col-sm-10 col-md-10 col-lg-10">
 			<div id='ninja-slider'>
-        <div>
-            <div class="slider-inner">
-                <ul>
-										@foreach($screenshot as $img)
-                    <li><a class="ns-img" href="{{$img->picture_path_name}}"></a></li>
-        						@endforeach
-                </ul>
-                <div class="fs-icon" title="Expand/Close"></div>
-            </div>
-            <div id="thumbnail-slider">
-                <div class="inner">
-                    <ul>
-												@foreach($screenshot as $img)
-                        <li>
-                            <a class="thumb" href="{{$img->picture_path_name}}"></a>
-                        </li>
-                        @endforeach
-                    </ul>
-                </div>
-            </div>
-        </div>
-   		</div>
+				<div>
+					<div class="slider-inner">
+						<ul>
+							@foreach($screenshot as $img)
+							<li><a class="ns-img" href="{{$img->picture_path_name}}"></a></li>
+							@endforeach
+						</ul>
+						<div class="fs-icon" title="Expand/Close"></div>
+					</div>
+					<div id="thumbnail-slider">
+						<div class="inner">
+							<ul>
+								@foreach($screenshot as $img)
+								<li>
+									<a class="thumb" href="{{$img->picture_path_name}}"></a>
+								</li>
+								@endforeach
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
 		</div>
 		<div class="col-xs-1 col-sm-1 col-md-1 col-lg-1"></div>
 	</div>
-  @endif
+	@endif
 </div>
 <script src="{!! URL::asset('js/ninja-slider.js') !!}"></script>
 <script src="{!! URL::asset('js/contact-buttons.js') !!}"></script>
 <script>
-$(document).ready(function() {
-        var str = $("div.tools").html();
+	$(document).ready(function() {
+		var str = $("div.tools").html();
 		$("div.tools").html(str.replace(/\n/g, "<br />"));
 
-    }
-);
-WebFontConfig = {
-  google: { families: [ 'Lato:400,700,300:latin' ] }
-};
-(function() {
-  var wf = document.createElement('script');
-  wf.src = ('https:' == document.location.protocol ? 'https' : 'http') +
-    '://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
-  wf.type = 'text/javascript';
-  wf.async = 'true';
-  var s = document.getElementsByTagName('script')[0];
-  s.parentNode.insertBefore(wf, s);
-})();
+	}
+	);
+	WebFontConfig = {
+		google: { families: [ 'Lato:400,700,300:latin' ] }
+	};
+	(function() {
+		var wf = document.createElement('script');
+		wf.src = ('https:' == document.location.protocol ? 'https' : 'http') +
+		'://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
+		wf.type = 'text/javascript';
+		wf.async = 'true';
+		var s = document.getElementsByTagName('script')[0];
+		s.parentNode.insertBefore(wf, s);
+	})();
 
 // Initialize Share-Buttons
 $.contactButtons({
-  effect  : 'slide-on-scroll',
-  buttons : {
-    'facebook':   { class: 'facebook', use: true, extras: 'target="_blank"' },
-    'twitter':    { class: 'twitter',   use: true, },
-    'linkedin':   { class: 'linkedin', use: true, },
-    'google':     { class: 'gplus',    use: true, },
-    'pinterest':  { class: 'pinterest', use: true, },
-    'email':      { class: 'email',    use: true, link: 'test@web.com' }
-  }
+	effect  : 'slide-on-scroll',
+	buttons : {
+		'facebook':   { class: 'facebook', use: true, extras: 'target="_blank"' },
+		'twitter':    { class: 'twitter',   use: true, },
+		'linkedin':   { class: 'linkedin', use: true, },
+		'google':     { class: 'gplus',    use: true, },
+		'pinterest':  { class: 'pinterest', use: true, },
+		'email':      { class: 'email',    use: true, link: 'test@web.com' }
+	}
 });
 </script>
 @stop
