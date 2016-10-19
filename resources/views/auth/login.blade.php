1effb0e2bfb6ec7717b68c65b3f52c944884d16e
evefyyyy <evefyjubpajub@gmail.com>
2016-10-19 23:31:18 +0700

    login

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,30 @@
+<html>
+<head>
+  <title>SIT Portfolio</title>
+  <link href="//fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+  <link href="{!! URL::asset('/css/login.css') !!}" rel="stylesheet" type="text/css">
+  <link href="{!! URL::asset('/css/style.css') !!}" rel="stylesheet" type="text/css">
+  <link href="{!! URL::asset('css/bootstrap.css') !!}" rel="stylesheet">
+  <script src="{!! URL::asset('js/jquery-2.2.4.js') !!}"></script>
+  <script src="{!! URL::asset('js/bootstrap.js') !!}"></script>
+  <script src="{!! URL::asset('js/login.js') !!}"></script>
+  <script src="{!! URL::asset('//cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js') !!}"></script>
+  <link href="//fonts.googleapis.com/css?family=Prompt:300" rel="stylesheet">
+  <link href='//fonts.googleapis.com/css?family=Roboto+Condensed:700,400' rel='stylesheet' type='text/css'>
+</head>
+<body>
+<div class="container">
+	<div class="login-container">
+        <div id="output"></div>
+        <img src="/img/logo.png">
+        <div class="form-box">
+            <form action="" method="">
+                <input type="text" placeholder="username">
+                <input type="password" placeholder="password">
+                <button class="btn-login" type="submit">Login</button>
+            </form>
+        </div>
+    </div>    
+</div>
+</body>
+</html>
