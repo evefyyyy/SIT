6b9da499e47167b0c1a50347bb20a56ff3df8e7e
llfangll <fang.ronaporn@gmail.com>
2016-10-22 00:24:22 +0700

    show project

--- resources/views/showProject.blade.php
+++ resources/views/showProject.blade.php
@@ -19,36 +19,37 @@
 	</div>
 	<div class="row">
 		<div class="col-xs-1 col-sm-1 col-md-1 col-lg-1"></div>
-		@if(Auth::user()->user_student != null)
-		<?php
-		$objs = Auth::user()->user_student->student->student_id;
-		$checkStd = DB::table('students')->where('student_id',$objs)->value('id');
-		$projectId = DB::table('project_students')->where('student_pkid',$checkStd)->value('project_pkid');
-		?>
-			@if($projectId == $checkProject)
-		<div class="col-xs-7 col-sm-7 col-md-7 col-lg-7">
-			<h3>{{$projectNameEN}}</h3>
-			<h4>{{$projectNameTH}}</h4>
-		</div>
-		<div class="col-xs-3 col-sm-3 col-md-3 col-lg-3 btn-save">
+		@if(Auth::check())
+			@if(Auth::user()->user_student != null)
+			<?php
+			$objs = Auth::user()->user_student->student->student_id;
+			$checkStd = DB::table('students')->where('student_id',$objs)->value('id');
+			$projectId = DB::table('project_students')->where('student_pkid',$checkStd)->value('project_pkid');
+			?>
+				@if($projectId == $checkProject)
+			<div class="col-xs-7 col-sm-7 col-md-7 col-lg-7">
+				<h3>{{$projectNameEN}}</h3>
+				<h4>{{$projectNameTH}}</h4>
+			</div>
+			<div class="col-xs-3 col-sm-3 col-md-3 col-lg-3 btn-save">
 
-			<form action="{{url('student/myproject/edit/'.$checkProject.'/edit')}}" method="get">
-				<button style="float:right" class="btn btn-browse" onclick="window.location.href='/student/myproject/edit'">edit my project</button>
-			</form>
-		</div>
+				<form action="{{url('student/myproject/edit/'.$checkProject.'/edit')}}" method="get">
+					<button style="float:right" class="btn btn-browse" onclick="window.location.href='/student/myproject/edit'">edit my project</button>
+				</form>
+			</div>
+				@else
+			<div class="col-xs-10 col-sm-10 col-md-10 col-lg-10">
+				<h3>{{$projectNameEN}}</h3>
+				<h4>{{$projectNameTH}}</h4>
+			</div>
+				@endif
 			@else
-		<div class="col-xs-10 col-sm-10 col-md-10 col-lg-10">
-			<h3>{{$projectNameEN}}</h3>
-			<h4>{{$projectNameTH}}</h4>
-		</div>
+			<div class="col-xs-10 col-sm-10 col-md-10 col-lg-10">
+				<h3>{{$projectNameEN}}</h3>
+				<h4>{{$projectNameTH}}</h4>
+			</div>
 			@endif
-		@else
-		<div class="col-xs-10 col-sm-10 col-md-10 col-lg-10">
-			<h3>{{$projectNameEN}}</h3>
-			<h4>{{$projectNameTH}}</h4>
-		</div>
 		@endif
-
 		<div class="col-xs-1 col-sm-1 col-md-1 col-lg-1"></div>
 	</div>
 	<div class="row">
