41b6270d61e1832176ce846d54788e9a1f275a35
evefyyyy <evefyjubpajub@gmail.com>
2016-10-22 16:45:01 +0700

    vote button

--- resources/views/showProject.blade.php
+++ resources/views/showProject.blade.php
@@ -31,7 +31,6 @@
 			<h4>{{$projectNameTH}}</h4>
 		</div>
 		<div class="col-xs-3 col-sm-3 col-md-3 col-lg-3 btn-save">
-			
 			<form action="{{url('student/myproject/edit/'.$checkProject.'/edit')}}" method="get">
 				<button style="float:right" class="btn btn-browse" onclick="window.location.href='/student/myproject/edit'">edit my project</button>
 			</form>
@@ -43,11 +42,16 @@
 		</div>
 			@endif
 		@else
-		<div class="col-xs-10 col-sm-10 col-md-10 col-lg-10">
+		<div class="col-xs-10 col-sm-7 col-md-8 col-lg-8">
 			<h3>{{$projectNameEN}}</h3>
 			<h4>{{$projectNameTH}}</h4>
 		</div>
-
+		<div class="col-xs-1 hidden-sm hidden-md hidden-lg"></div>
+		<div class="col-xs-12 hidden-sm hidden-md hidden-lg"></div>
+		<div class="col-xs-1 hidden-sm hidden-md hidden-lg"></div>
+		<div class="col-xs-10 col-sm-3 col-md-2 col-lg-2">
+			<a href="/dday" class="btn3 btn-3 btn-3e">vote<i class="glyphicon glyphicon-star"></i></a>
+		</div>
 		@endif
 		<div class="col-xs-1 col-sm-1 col-md-1 col-lg-1"></div>
 	</div>
