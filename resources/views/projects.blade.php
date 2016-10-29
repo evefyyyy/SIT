999dbe8f72e36c92018bd49c6a829858b8801008
evefyyyy <evefyjubpajub@gmail.com>
2016-10-29 23:27:48 +0700

    allproject

--- resources/views/projects.blade.php
+++ resources/views/projects.blade.php
@@ -15,8 +15,8 @@
         </div>
     </div>
 <div class="row">
-	<div class="hidden-xs col-sm-1 col-md-1 col-lg-1"></div>
-		<div class="col-xs-12 col-sm-10 col-md-10 col-lg-10">
+  <div class="hidden-xs col-sm-1 col-md-1 col-lg-1"></div>
+    <div class="col-xs-12 col-sm-10 col-md-10 col-lg-10">
 
       @foreach($groupProject as $project)
       <?php
@@ -30,38 +30,37 @@
                     ->where('id',$catId)
                     ->value('category_name');
        ?>
-			<div class="col-xs-18 col-sm-6 col-md-4 col-lg-3" data-tag='{{$category}}'>
-	          <div class="thumbnail">
-	          	<div class="pdf-thumb-box">
-			      <a href="/showproject/{{$project->group_project_id}}">
-			      	 <div class="pdf-thumb-box-overlay">
-			        	<div class="center-box"></div><i class="glyphicon glyphicon-eye-open gi-2x"></i>
-       				 </div>
+      <div class="col-xs-18 col-sm-6 col-md-4 col-lg-3" data-tag='{{$category}}'>
+            <div class="thumbnail">
+              <div class="pdf-thumb-box">
+            <a href="/showproject/{{$project->group_project_id}}">
+               <div class="pdf-thumb-box-overlay">
+                <div class="center-box"></div><i class="glyphicon glyphicon-eye-open gi-2x"></i>
+               </div>
               @if(count($poster)===0)
               <img src="/img/no-poster.png">
               @else
-	          <img src="{{$poster}}" alt="">
+            <img src="{{$poster}}" alt="">
               @endif
-          		</a>
-          		</div>
-	              <div class="caption">
-	                <h6>{{$project->group_project_eng_name}}</h6>
-	                   <p>{{$project->group_project_detail}}</p>
-	            </div>
-	       	  </div>
-	       	</div>
+              </a>
+              </div>
+                <div class="caption">
+                  <h6>{{$project->group_project_eng_name}}</h6>
+                     <p>{{$project->group_project_detail}}</p>
+              </div>
+            </div>
+          </div>
           @endforeach
-	<div class="hidden-xs col-sm-1 col-md-1 col-lg-1"></div>
+  <div class="hidden-xs col-sm-1 col-md-1 col-lg-1"></div>
 </div>
-	<script>
-		$(document).ready(function() {
-		    $("div.caption").dotdotdot(
-		    {
-		        ellipsis : '...',
-		        wrap: 'word',
-            fallbackToLetter: true,
-		        height: 60,
-		    });
-		});
-	</script>
+  <script>
+    $(document).ready(function() {
+        $("div.caption").dotdotdot(
+        {
+            ellipsis : '...',
+            wrap: "word",
+            height: 60,
+        });
+    });
+  </script>
 @stop
