7ba967e4617d66478dd66eb78cb429bb38d78b3c
nookotto <undernook@hotmail.com>
2016-11-05 17:00:28 +0700

    push

---
+++ database/migrations/2012_11_05_164138_create_score_level_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateScoreLevelTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('score_level', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('score_level_name');
+            $table->integer('score');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
