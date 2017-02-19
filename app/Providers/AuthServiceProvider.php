de94e9d78ab8eb9dad755630625521e5669c58b2
evefyyyy <evefyjubpajub@gmail.com>
2017-02-19 20:01:47 +0700

    update laravel

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -22,10 +22,10 @@ class AuthServiceProvider extends ServiceProvider
      * @param  \Illuminate\Contracts\Auth\Access\Gate  $gate
      * @return void
      */
-    public function boot(GateContract $gate)
-    {
-        $this->registerPolicies($gate);
+    // public function boot(GateContract $gate)
+    // {
+    //     $this->registerPolicies($gate);
 
-        //
-    }
+    //     //
+    // }
 }
